b9d9c042cad347b3dceb301ad2d1301cc37bbee5
M4dCh34t3r <LegendPotty@gmail.com>
2022-07-27 10:55:14 -0300

    feat: add '/reviews' route to 'routes.tsx'

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { CartContent } from "./pages/cart-content";
+import { Reviews } from "./pages/reviews";
 import { Home } from "./pages/home";
 
 const mainRoutes = () => {
   return(
     <Routes>
       <Route
-          path="/"
-          element={<Home />} />
+        path="/"
+        element={ <Home /> } />
       <Route
-          path="/cart-content"
-          element={<CartContent />} />
+        path="/cart-content"
+        element={ <CartContent /> } />
+      <Route
+        path="/reviews"
+        element={ <Reviews /> } />
     </Routes>
   );
 }
