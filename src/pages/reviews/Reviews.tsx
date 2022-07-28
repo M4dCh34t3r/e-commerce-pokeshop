943f34bf08531ae2e7219af3200d77a4e499525e
M4dCh34t3r <LegendPotty@gmail.com>
2022-07-27 21:09:58 -0300

    feat: add 'Entry' component to 'Revies' page

--- src/pages/reviews/Reviews.tsx
+++ src/pages/reviews/Reviews.tsx
@@ -1,35 +1,80 @@
 import { ClientReview } from '../../data/@types/clientReview';
 import Messages from '../../components/messages/Messages';
 import reviews from "../../data/json/reviews.json";
+import Entry from '../../components/entry/Entry';
 import { useState, useEffect } from 'react';
 
 export function Reviews() {
-    const [content, setContent] = useState<ClientReview[]>([]);
-    const url = 'http://localhost:5000/reviews';
+  const [content, setContent] = useState<ClientReview[]>([]);
+  const url = 'http://localhost:5000/reviews';
 
-    useEffect(() =>  {
-      const fetchData = async () => {
-        try {
-          let response = await fetch(url);
+  const [authorName, setAuthorName] = useState('');
+  const [messageContent, setMessageContent] = useState('');
 
-          if (response.status === 200) {
-            console.log("Data successfully fetched from ", url);
-            let data = await response.json();
-            setContent(data);
-            return
-          }
-          throw 'Error fetching reviews';
+  useEffect(() =>  {
+    const fetchData = async () => {
+      try {
+        let response = await fetch(url);
 
-        } catch(e: any) {
-          console.log(e);
-          console.log('Fetching data from "src/data/reviews/reviews.json"')
-          setContent(reviews);
+        if (response.status === 200) {
+          console.log("Data successfully fetched from ", url);
+          let data = await response.json();
+          setContent(data);
+          return
         }
+      } catch {
+        console.log('Fetching data from "src/data/reviews/reviews.json"')
+        setContent(reviews);
       }
-      fetchData();
-    });
+    }
+    fetchData();
+  });
+
+  const sendReview = () => {
+
+    if (authorName === '') {
+      alert("Please, insert an author name.");
+      return;
+    }
+
+    if (messageContent === '') {
+      alert("Please, insert a message");
+      return;
+    }
+
+    const postBody = {
+      author: authorName,
+      message: messageContent
+    }
+
+    const fetchData = async () => {
+      try {
+        await fetch(url,
+          { method: 'POST',
+          headers: { 'content-type': 'application/json' },
+          body: JSON.stringify(postBody) });
+      } catch(e: any) {
+        console.log(e);
+      }
+    }
+    fetchData();
+  }
+
+    const updateAuthor = (nextAuthor: string) => {
+      setAuthorName(nextAuthor);
+    }
+
+    const updateMessage = (nextMessage: string) => {
+      setMessageContent(nextMessage);
+    }
 
   return(
-    <Messages clientReviews={ content }/>
+    <>
+      <Entry 
+        clickHandler={ sendReview }
+        nameHandler={ updateAuthor }
+        messageHandler={ updateMessage } />
+      <Messages clientReviews={ content } />
+    </>
   );
 }
